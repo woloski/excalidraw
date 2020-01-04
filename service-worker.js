e324cdca5b3c0f8efb9d40d696760f922d6e9e5d
vjeux <vjeuxx@gmail.com>
2020-01-04 02:17:30 +0000

    Deploying to gh-pages from  929efa5e2c07955e956525c05f2c869aecffdba3

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9efa9bbd423329d615f38caa099d88e0.js"
+  "/precache-manifest.597aa4dc494d2338046349c0ad12557f.js"
 );
 
 self.addEventListener('message', (event) => {
