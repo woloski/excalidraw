9e08b3d868936acc99a35d20f6aac2facb439f71
GasimGasimzada <me@gasim.xyz>
2020-01-06 15:35:29 +0000

    Deploying to gh-pages from  01805f734d87b56b60febe56f33790cd744a3fac

---
+++ precache-manifest.48d8480def227d538b1e11e12a6b91f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8d74e0176ecf490398a60b9aad12d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7d70dcacaa67aef0560",
+    "url": "/static/css/main.afb0fc9a.chunk.css"
+  },
+  {
+    "revision": "f71861b7208eedf7cf29",
+    "url": "/static/js/2.08dfaaa7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.08dfaaa7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7d70dcacaa67aef0560",
+    "url": "/static/js/main.cad43afc.chunk.js"
+  },
+  {
+    "revision": "b44dee203db29ecae5db",
+    "url": "/static/js/runtime-main.b019aae8.js"
+  }
+]);
